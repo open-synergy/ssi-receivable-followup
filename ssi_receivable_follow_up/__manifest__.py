5e33f8e8df3c4139c22ade471a6acfe44fc9c7d2
mikevhe18 <viriyananda.michael@gmail.com>
2023-08-21 11:14:34 +0700

    [ssi_receivable_follow_up] Change License To AGPL-3

--- ssi_receivable_follow_up/__manifest__.py
+++ ssi_receivable_follow_up/__manifest__.py
@@ -1,14 +1,14 @@
 # Copyright 2023 OpenSynergy Indonesia
 # Copyright 2023 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Receivable Follow Up",
     "version": "14.0.1.0.1",
     "category": "Accounting",
     "website": "https://simetri-sinergi.id",
-    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
-    "license": "LGPL-3",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
     "installable": True,
     "depends": [
         "ssi_financial_accounting",
--- ssi_receivable_follow_up/models/receivable_follow_up_detail.py
+++ ssi_receivable_follow_up/models/receivable_follow_up_detail.py
@@ -1,6 +1,6 @@
 # Copyright 2023 OpenSynergy Indonesia
 # Copyright 2023 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
 
@@ -23,13 +23,11 @@ class ReceivableFollowUpDetail(models.Model):
     )
     partner_id = fields.Many2one(
         string="Partner", related="invoice_id.partner_id", store=True
-    )    
+    )
     currency_id = fields.Many2one(
         string="Currency", related="invoice_id.currency_id", store=True
     )
-    date_invoice = fields.Date(
-        string="Date", related="invoice_id.date", store=True
-    )
+    date_invoice = fields.Date(string="Date", related="invoice_id.date", store=True)
     date_due = fields.Date(
         string="Date Due", related="invoice_id.date_maturity", store=True
     )
@@ -55,11 +53,18 @@ class ReceivableFollowUpDetail(models.Model):
             result = 0.0
             criteria = [
                 ("debit_move_id", "=", record.invoice_id.id),
-                ("credit_move_id.date", ">=", fields.Date.to_string(record.follow_up_id.date_start)),
-                ("credit_move_id.date", "<=", fields.Date.to_string(record.follow_up_id.date_end)),
+                (
+                    "credit_move_id.date",
+                    ">=",
+                    fields.Date.to_string(record.follow_up_id.date_start),
+                ),
+                (
+                    "credit_move_id.date",
+                    "<=",
+                    fields.Date.to_string(record.follow_up_id.date_end),
+                ),
             ]
             for line in self.env["account.partial.reconcile"].search(criteria):
                 result += abs(line.credit_move_id.amount_currency)
             record.amount_collected = result
             record.amount_diff = record.amount_residual - record.amount_collected
-
